9d67b11f7126994ab6ea32c313d8fa087ab7a66d
Lewis Marshall <lewis@lmars.net>
2016-07-18 21:22:10 +0100

    Refactor index.js
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,29 @@
-$(function() {
+(function() {
+  "use strict";
+
+  var loading  = $("#loading")
+  var alertBox = $(".alert")
+
   $(document).ajaxError(function(event, jqxhr, settings, error) {
-    $("#loading").hide()
+    loading.hide()
 
     var msg = settings.type + " " + settings.url + " Error!"
 
-    $(".alert").removeClass("hide").find("p").text(msg)
+    alertBox.removeClass("hide").find("p").text(msg)
   })
 
+  var channelsTable  = $("#channels tbody")
+  var container      = $(".container")
   var renderTableRow = _.template($("#table-row-template").html())
   var renderHistory  = _.template($("#history-template").html())
 
   $.getJSON("/api/channels", function(channels) {
-    $("#loading").hide()
+    loading.hide()
 
     $.each(channels, function(_, channel) {
-      $("#channels tbody").append(renderTableRow(channel))
+      channelsTable.append(renderTableRow(channel))
 
-      $(".container").append(renderHistory(channel))
+      container.append(renderHistory(channel))
     })
   })
-})
+})()
